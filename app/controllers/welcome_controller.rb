5cae90ab883cdafab48f812e09d77fd08f3087e7
Imran Khoja <imrankhoja1@gmail.com>
2013-11-15 19:04:23 -0500

    initial blog version

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+#write a random method and call it sarah
+  def sarah
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '80107ec995cd7cbfa470e7759e5a089a059aabea0481471b077570af78cc372d52b01043cfc8fe460a017e97241eaaf96113ef616ecc3c94f77a05dd395ce3d0'
