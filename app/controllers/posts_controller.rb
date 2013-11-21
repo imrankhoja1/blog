a0727b5299f1433535510a33e9a7bcb519eb622a
Imran Khoja <imrankhoja1@gmail.com>
2013-11-20 19:49:54 -0500

    Finish up blog project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
+	
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
 	def new
+		@post = Post.new
 	end
 	def create
 		@post = Post.new(post_params)
-  		@post.save
-  		redirect_to @post
+  		
+  		if @post.save
+  			redirect_to @post
+  		else
+  			render 'new'
+  		end
 	end
 
 	def show
@@ -13,6 +21,27 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 	private
